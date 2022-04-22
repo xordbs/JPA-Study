145b810812b5e4bb893f68172da545d2604b85ce
xordbs <xoem00@gmail.com>
2022-04-22 22:22:53 +0900

    add swagger setting

---
+++ study1/src/main/java/com/xordbs/study1/Hello.java
@@ -0,0 +1,9 @@
+package com.xordbs.study1;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Hello {
+    private String data;
+}
--- study1/src/main/java/com/xordbs/study1/Study1Application.java
+++ study1/src/main/java/com/xordbs/study1/Study1Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Study1Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Study1Application.class, args);
+
     }
 
 }

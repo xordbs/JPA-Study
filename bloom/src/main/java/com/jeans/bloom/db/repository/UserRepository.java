2adf854f987b5e395c84d15f598516b780be4f29
오윤택 <xoem00@gmail.com>
2022-04-27 22:43:56 +0900

    Bloom PJT

---
+++ bloom/src/main/java/com/jeans/bloom/db/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.jeans.bloom.db.repository;
+
+import com.jeans.bloom.db.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * OYT | 2022.04.27
+ * @name UserRepository
+ * @des user 테이블 사용을 위한 JpaRepository
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    User findUserByUserId(String userId);
+
+    User findUserByNickName(String nickName);
+}
